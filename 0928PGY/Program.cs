7c04c8f1e538a990eeb951978dddbcff3819643b
DO2911 <rlg22401@gmail.com>
2020-09-28 08:44:08 +0200

    Add project files.

---
+++ 0928PGY/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0928PGY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random rnd = new Random();
+            int d = 0;
+            int f = rnd.Next(0, 999);
+            do
+            {
+                d++;
+                Console.WriteLine($"Hello World! {d}");
+            } while (d!=f);
+            Console.ReadKey();
+
+        }
+    }
+}
